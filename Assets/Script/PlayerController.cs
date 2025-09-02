39f3d3751a64e645aca0f1f8091e8825908d3f73
TynniBru <brunnorossi22@gmail.com>
2025-09-02 12:18:49 -0300

    Variaveis atualizadas

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private Vector2 p_Dir;
+    private Vector2 moveDir;
 
     [Header("Movimentação")]
-    [SerializeField] public float p_spd = 5;
-    [SerializeField] public float max_spd = 10;
-    [SerializeField] public float acc = 2;
-    [SerializeField] public float dcc = 2;
+    [SerializeField] public float moveSpd = 5;
+    [SerializeField] public float maxSpd = 10;
+    [SerializeField] public float accel = 2;
+    [SerializeField] public float deccel = 2;
     [SerializeField] public float TurnVel = 3;
 
     void Awake()
@@ -19,9 +19,9 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        p_Dir.x = Input.GetAxisRaw("Horizontal");
-        p_Dir.y = Input.GetAxisRaw("Vertical");
-        p_Dir.Normalize();
+        moveDir.x = Input.GetAxisRaw("Horizontal");
+        moveDir.y = Input.GetAxisRaw("Vertical");
+        moveDir.Normalize();
         RotatePlayer();
     }
 
@@ -32,25 +32,25 @@ public class PlayerController : MonoBehaviour
 
     void HandleMovement()
     {
-        Vector2 targetVelocity = p_Dir * p_spd;
+        Vector2 targetVelocity = moveDir * moveSpd;
         Vector2 force = Vector2.zero;
-        if (p_Dir.magnitude > 0)
+        if (moveDir.magnitude > 0)
         {
-            force = (targetVelocity - rb.linearVelocity) * acc;
+            force = (targetVelocity - rb.linearVelocity) * accel;
         }
         else
         {
-            force = (targetVelocity - rb.linearVelocity) * dcc;
+            force = (targetVelocity - rb.linearVelocity) * deccel;
         }
         rb.AddForce(force);
-        rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, max_spd);
+        rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, maxSpd);
     }
 
     void RotatePlayer()
     {
-        if (p_Dir != Vector2.zero)
+        if (moveDir != Vector2.zero)
         {
-            float angle = Mathf.Atan2(p_Dir.y, p_Dir.x) * Mathf.Rad2Deg;
+            float angle = Mathf.Atan2(moveDir.y, moveDir.x) * Mathf.Rad2Deg;
             Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, TurnVel * Time.deltaTime);
         }
