9d6de1356b3f69c120327691273addadee0cc497
Kaualvz <ka597749@gmail.com>
2025-09-25 17:29:47 -0300

    Protótipo do menu
    protótipo do menu inicial, e do menu de opções

--- Assets/Script/MenuManager.cs
+++ Assets/Script/MenuManager.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; // ESTE É O IMPORTANTE!
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Esta função será chamada pelo botão para trocar a cena.
+    public void CarregarJogo()
     {
-        
+        // Certifique-se de que "NomeDaCenaDoJogo" é o nome exato do seu arquivo de cena.
+        SceneManager.LoadScene("Teste");
+
+        // Dica: Você também pode usar SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+        // para carregar a próxima cena na ordem de construção, mas usar o nome é mais seguro.
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SairDoJogo()
     {
-        
+        Application.Quit();
+        Debug.Log("Saindo do Jogo..."); // Mostra no console da Unity, mas só fecha no build final.
     }
 }
